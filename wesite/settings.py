2c85e9c9983c69c2dd8fdaa28993bc2f7a36a025
hueila <hueila_14@hotmail.com>
2021-09-17 13:12:24 -0300

    Instalado o Procfile

--- wesite/settings.py
+++ wesite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8n_t%-l!zh-a0mutp1l1jlz!o*et0fqg90jpz(m@e#d%^wd^h)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
