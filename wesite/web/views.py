479574409e9df58526da35d79f8c2d1dc2781cdf
hueila <hueila_14@hotmail.com>
2021-10-09 23:20:35 -0300

    Corrigido infra de testes com pytest-django_#20

--- wesite/web/views.py
+++ wesite/web/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><body>Ola, Hueila Saraiva de Sousa !!!</body></html>', content_type='text/html')
+    return HttpResponse('Olá, Hueila Saraiva de Sousa !!!')
 
 
